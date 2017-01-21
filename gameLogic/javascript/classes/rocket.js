a0308dde0fbbf6c3151230919af057e1703bb87e
Sarah Drumm <Sarah Drumm>
2017-01-21 12:57:16 -0500

    Updating naming error on rocket png

--- gameLogic/javascript/classes/rocket.js
+++ gameLogic/javascript/classes/rocket.js
@@ -1,7 +1,7 @@
 class Rocket extends Base {
 
   constructor(centerX, centerY, width, height) {
-    super(centerX, centerY, width, height, 0, 0, "RocketShip");
+    super(centerX, centerY, width, height, 0, 0, "rocket");
   }
 
   collideWithAsteroid(asteroids) {
