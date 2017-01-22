2194d9c0257f687f18ed4b5036aa915b4646ec80
Sarah Drumm <Sarah Drumm>
2017-01-21 23:12:44 -0500

    Adding rocket updating

--- gameLogic/javascript/firebase.js
+++ gameLogic/javascript/firebase.js
@@ -11,12 +11,6 @@ function setAstronauts(centerX, centerY, angle, astronautId) {
   });
 };
 
-function setRocket() {
-  firebase.database().ref('rocket/' + '/powers/' + powerName).set({
-    valid: true
-  });
-};
-
 function setHammers(centerX, centerY, angle, astronautId, startTime, hammerId) {
   firebase.database().ref('hammers/' + hammerId).set({
     centerX: centerX,
@@ -25,7 +19,7 @@ function setHammers(centerX, centerY, angle, astronautId, startTime, hammerId) {
     astronautId: astronautId,
     startTime: startTime
   });
-}
+};
 
 function setAsteroids(centerX, centerY, angle, astronautId, startTime, size, type, speed, asteroidId) {
   firebase.database().ref('asteroids/' + asteroidId).set({
@@ -38,8 +32,14 @@ function setAsteroids(centerX, centerY, angle, astronautId, startTime, size, typ
     type: type,
     speed: speed
   });
-}
+};
 
+function setRocket() {
+  firebase.database().ref('rocket/powers/oxygen-refill').set(astronautId);
+  firebase.database().ref('rocket/health/').set(health);
+};
+
+	
 //Update from space to DB
 
 function updateAstronauts(centerX, centerY, angle, astronautId) {
