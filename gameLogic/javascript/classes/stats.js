a1f98330ead3516d590f62fe695cbcaa32bd26ae
Josh Clyde <joshclyde11@gmail.com>
2017-01-22 06:34:40 -0500

    Make oxygen bar more pretty colors

--- gameLogic/javascript/classes/stats.js
+++ gameLogic/javascript/classes/stats.js
@@ -9,11 +9,29 @@ class Stats {
   update() {
   }
 
+  getColor(oxygen) {
+    // green for first half
+    if (oxygen > 50) {
+      return "#32CD32";
+    } else if (oxygen > 30) {
+      return "#ffff00";
+    } else {
+      return "#ff0000";
+    }
+  }
+
   draw(canvas, oxygen) {
-    canvas.fillStyle = "#000000"; // make oxygen pure red
+    // make oxygen background black
+    canvas.fillStyle = "#000000";
     canvas.fillRect(this.left, this.top, this.width, this.height);
-    canvas.fillStyle = "#ff0000"; // make oxygen pure red
+    // draw
+    canvas.fillStyle = this.getColor(oxygen); // make oxygen pure red
     canvas.fillRect(this.left, this.top, this.width * oxygen / 100, this.height);
+    if (oxygen < 30) {
+      canvas.fillStyle = "rgba(255, 255, 255, " + (oxygen % 2) / 2 + ")";
+      canvas.fillRect(this.left, this.top, this.width * oxygen / 100, this.height);
+    }
+    // write oxygen
     canvas.fillStyle = "#ffffff";
     canvas.textAlign = "center";
     canvas.font = "18px serif";
